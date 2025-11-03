07d742ce708d4f4e509a4f407096e327a81f34c4
Lolkinglolof <Torben.Thiemer@gmail.com>
2025-11-03 14:52:38 +0100

    movement is quite functional

--- FlyMouse/Assets/Scripts/CharacterController.cs
+++ FlyMouse/Assets/Scripts/CharacterController.cs
@@ -5,7 +5,7 @@ public class CharacterController : MonoBehaviour
 
     [SerializeField] Vector2 direction;
     [SerializeField] KeyCode left = KeyCode.A, right = KeyCode.D;
-    [SerializeField] float gravityTimeToZero, turnspeed, minimalGravityEffect;
+    [SerializeField] float gravityTimeToTurnZero, turnspeed, minimalGravityTurnEffect, movementSpeed, momentum, gravityConstant, momentumLossRate, maxMomentum;
     [SerializeField] GameObject directionFinder;
     // Start is called before the first frame update
     void Start()
@@ -17,6 +17,12 @@ public class CharacterController : MonoBehaviour
     void Update()
     {
         direction = directionFinder.transform.position - transform.position;
+        InputTaker();
+        GravityTurnCalculator();
+        Movement();
+    }
+    void InputTaker()
+    {
         if (Input.GetKey(left) && Input.GetKey(right))
         {
 
@@ -29,24 +35,43 @@ public class CharacterController : MonoBehaviour
         {
             transform.Rotate(Vector3.forward, turnspeed * Time.deltaTime);
         }
-        else
-        {
-
-        }
-        if (Vector2.Angle(direction, Vector2.down) < minimalGravityEffect * Time.deltaTime)
+    }
+    void GravityTurnCalculator()
+    {
+        if (Vector2.Angle(direction, Vector2.down) < minimalGravityTurnEffect * Time.deltaTime)
         {
             if (Vector2.SignedAngle(direction, Vector2.down) < 0)
             {
-                transform.Rotate(Vector3.forward, -minimalGravityEffect * Time.deltaTime);
+                transform.Rotate(Vector3.forward, -minimalGravityTurnEffect * Time.deltaTime);
             }
             else if (Vector2.SignedAngle(direction, Vector2.down) > 0)
             {
-                transform.Rotate(Vector3.forward, minimalGravityEffect * Time.deltaTime);
+                transform.Rotate(Vector3.forward, minimalGravityTurnEffect * Time.deltaTime);
             }
         }
         else
         {
-            transform.Rotate(Vector3.forward, Vector2.SignedAngle(direction, Vector2.down) / gravityTimeToZero * Time.deltaTime);
+            transform.Rotate(Vector3.forward, Vector2.SignedAngle(direction, Vector2.down) / gravityTimeToTurnZero * Time.deltaTime);
         }
+    }
+    void Movement()
+    {
+        
+        transform.position = (Vector2)transform.position + momentum * movementSpeed * Time.deltaTime * direction;
+        Vector3 previousPosition = transform.position;
+        GravityMovement();
+        float tempMomentum = Mathf.Clamp(Mathf.Log((transform.position - previousPosition).magnitude / Time.deltaTime, 2), 1, maxMomentum);
+        if (tempMomentum > momentum)
+        {
+            momentum = tempMomentum;
+        }
+        else
+        {
+            momentum -= momentumLossRate * Time.deltaTime;
+        }
+    }
+    void GravityMovement()
+    {
+        transform.position = (Vector2)transform.position + gravityConstant * Mathf.Lerp(0.1f, 1f, Mathf.Abs(direction.y)) * Vector2.down;
     }
 }
