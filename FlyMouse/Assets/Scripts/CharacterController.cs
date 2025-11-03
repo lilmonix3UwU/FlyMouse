664006a5275e478cf3bebc60dac91a1751a65cb8
Lolkinglolof <Torben.Thiemer@gmail.com>
2025-11-03 09:49:40 +0100

    start of the player controller

---
+++ FlyMouse/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CharacterController : MonoBehaviour
+{
+    public InputActionAsset actions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
