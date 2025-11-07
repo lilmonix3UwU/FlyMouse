77578d776fa534ce6eb78113b9a018c3650108b4
lilmonix3UwU <Tbnielsen2005@gmail.com>
2025-11-07 13:34:58 +0100

    timer stuff

--- FlyMouse/Assets/Scripts/ScoreTimer.cs
+++ FlyMouse/Assets/Scripts/ScoreTimer.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEditor;
 using UnityEngine;
 
 public class ScoreTimer : MonoBehaviour
 {
     private float seconds;
+    private int minutes;
+
     public static ScoreTimer instance;
     [SerializeField] TMP_Text timer;
     private void Start()
@@ -16,10 +19,17 @@ public class ScoreTimer : MonoBehaviour
     void Update()
     {
         seconds += Time.deltaTime;
-        float roundedTimer = Mathf.Round((seconds - 1) * 100) / 100;
-        timer.text = roundedTimer.ToString();
-       // if (timer)
 
+        float roundedTimer = Mathf.Round((seconds - 1) * 100) / 100;
+
+        if (roundedTimer > 60)
+        {
+            seconds -= 60;
+            minutes++;
+        }
+
+        if (minutes == 0) timer.text = roundedTimer.ToString();
+        if (minutes > 0) timer.text = minutes + ":" + roundedTimer.ToString();
     }
     public float StopTimer()
     {
